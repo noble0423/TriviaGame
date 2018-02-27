54980e510965d0a88de224306363391608d59ae5
noble0423 <ryan.noble82@gmail.com>
2018-02-27 00:58:09 -0800

    final save prior to HW submission. still need to write code for the reset button, and fix timer panel when screen is shrunken

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# TriviaGame
+# Survivor Trivia Game
+
+## Objective
+
+In this game you will be asked 20 timed, trivia questions regarding the TV show, Survivor.
+
+### Instructions
+
+* Click the blue "play" button to begin the game.
+
+    * On the screen will appear 20 consectuctive questions each allowing 15 seconds for you to select your answer..
+
+        * Read each question and the corresponding answers carefully as you only get to answer once. Once you are ready to submit your answer, click on the corresponding letter button.
+
+            * Answer correctly and the game will move onto the next question.
+
+            * Answer incorrectly, and the game will move along to the next question.
+
+            * Should the question time expire and you have not made a choice, the game will also move onto the next question.
+    
+    * After you have gone through all 20 questions, you will see a game over screen detailing the total questions answered correctly, incorrectly, and those questions that went unanswered.
+
+        * Click restart button to play again.
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,80 @@
 //============================================================================================
 
 var questions = [
-    {text: "1. The equator passes through ?", choices: ["A. Ethiopia", "B. Kenya", "C. Nigeria", "D. Sudan"], answer: "b"},
+    {text: "1. Which Survivor earned the nickname 'Robfather'?", choices: ["A. Robert DeCanio", "B. Rob Mariano", "C. Rob Cesternino", "D. Robb Zbacnik"], answer: "B"},
     
-    {text: "2. International date line passes through ?", 
-    choices: ["A. Baffin bay", "B. Bering strait", "C. Denmark strait", "D. Hudson bay"], 
-    answer: "b"},
+    {text: "2. In Survivor Pearl Islands the season's villain, Jon Dalton, was known by what nickname?", 
+    choices: ["A. Jon Boy", "B. Johnny Fairplay", "C. The General", "D. Johnny Rotten"], 
+    answer: "B"},
     
-    {text: "3. The highest average salinity amongst the following seas is reported from ?", 
-    choices: ["A. Dead Sea", "B. Red Sea", "C. Black Sea", "D. Mediterranean Sea"], 
-    answer: "a"},
+    {text: "3. In Survivor Outback, Michael Skupin air-lifted out b/c he: ", 
+    choices: ["A. was burned by the fire", "B. was stung by a jellyfish", "C. broke his ankle", "D. was cut by a knife"], 
+    answer: "A"},
     
-    {text: "4. Great barrier reef is situated along the coast of ?", 
-    choices:["A. USA", "B. UK", "C. UK", "D. Australia"], 
-    answer: "d"},
+    {text: "4. Which Survivor winner has never won an individual immunity challenge?", 
+    choices:["A. Sophie Clarke", "B. Kim Spradlin", "C. Bob Crowley", "D. Sandra Diaz-Twine"], 
+    answer: "D"},
     
-    {text: "5. Which one of the following is a Land-locked country?", 
-    choices:["A. Cambodia", "B. Thailand", "C. Laos", " D. Vietnam"], 
-    answer: "c"},
+    {text: "5. Which contestant is the only person to have been voted out first TWICE??", 
+    choices:["A. Marissa Calihan", "B. Carolina Eastwood", "C. Francesca Hogi", " D. Semhar Tadesse"], 
+    answer: "C"},
+
+    {text: "6. Which Survivor villain was quoted saying 'I am your friend, but if you f*** with me, you're dead?'", 
+    choices:["A. Jerri Manthey", "B. Courtney Yates", "C. Abi-Maria Gomes", " D. Sandra Diaz-Twine"], 
+    answer: "C"},
+
+    {text: "7. Which season of Survivor is the only season to last longer than 39 days?", 
+    choices:["A. Survivor: Australia", "B. Survivor: Borneo", "C. Survivor: Guatemala", " D. Survivor: Samoa"], 
+    answer: "A"},
+
+    {text: "8. Who won Survivor: San Juan Del Sur?", 
+    choices:["A. Nadiya Andersen", "B. Baylor Wilson", "C. Missy Payne", " D. Natalie Anderson"], 
+    answer: "D"},
+
+    {text: "9. Which contestant holds the record for most days played on Survivor", 
+    choices:["A. Parvati", "B. Ozzy", "C. Amanda Kimmel", " D. Boston Rob"], 
+    answer: "B"},
+
+    {text: "10. Colby Donaldson, Terry Dietz, and Ozzy all hold the record for most immunity challenges won on a single season?",choices:["A. 4", "B. 5", "C. 6", " D. 7"], 
+    answer: "B"},
+    
+    {text: "11. In season 34 - Game Changers, Tai found/played how many hidden immunity idols?", choices: ["A. 0", "B. 4", "C. 3", "D. 2"], answer: "C"},
+    
+    {text: "12. Who did Tony beat in season 28 - Survivor Cagayan?", 
+    choices: ["A. Woo", "B. Kass", "C. Spencer", "D. Tasha"], 
+    answer: "A"},
+    
+    {text: "13. Beating out Gervase and Monica Culpepper, who was the sole Survivor of season 27 - Blood vs. Water?", 
+    choices: ["A. Tina Wesson", "B. Vytas", "C. Aras", "D. Tyson"], 
+    answer: "D"},
+    
+    {text: "14. Survivor: Micronesia - Fans vs. Favorites was won by?", 
+    choices:["A. Sandra Diaz-Twine", "B. Richard Hatch", "C. Parvati", "D. Cochran"], 
+    answer: "C"},
+    
+    {text: "15. Who holds the record for oldest sole survivor at 57 years old?", 
+    choices:["A. Bob Crowley-Gabon", "B. Kim Johnson-Africa", "C. Tom Westman-Palau", " D. Jan Gentry-Thailand"], 
+    answer: "A"},
+
+    {text: "16. Who holds the record for most times sent to Exile Island?", 
+    choices:["A. Danielle-Panama", "B. Jeremy-San Juan Del", "C. Jessica Kiper-Gabon", " D. Terry Dietz-Panama"], 
+    answer: "C"},
+
+    {text: "17. Who is the oldest player ever to compete on Survivor?", 
+    choices:["A. Rudy-Borneo/All-Stars", "B. Joe Del Campo-Kaoh Rong", "C. Jimmy Johnson-Nicaragua", " D. Tarzan-One World"], 
+    answer: "A"},
+
+    {text: "18. Big Tom from Survivor-Africa holds the record for most weight lost. How much did he lose?", 
+    choices:["A. 48 lbs.", "B. 60 lbs.", "C. 71 lbs.", " D. 80 lbs."], 
+    answer: "D"},
+
+    {text: "19. What season is considered the most brutal with 3 medical evacuations?", 
+    choices:["A. Blood vs. Water", "B. Kaoh Rong", "C. Austalian Outback", " D. China"], 
+    answer: "B"},
+
+    {text: "20. Which contestant is has competed on Survivor on 4 seperate seasons?", 
+    choices:["A. Coach", "B. Penner", "C. Cirie", " D. Amanda Kimmel"], 
+    answer: "C"},
 ];
 var selectedQuestion = "";
 // Variable to hold the index of current question.
@@ -34,7 +91,7 @@ var incorrectAnswers = 0;
 var unansweredAnswers = 0;
 
 // Variable: time per question
-var number = 10;
+var number = 15;
 
 // Variable used for the setInterval/clearInterval
 var intervalId;
@@ -52,11 +109,9 @@ function run() {
 // Function to determine the countdown decrement
 function decrement() {
     number--;
-    console.log(number);
     $("#display").html("<h1>" + number + "</h1>");
     if (number === 0) {
         unansweredAnswers++;
-        console.log("questions unanswered " + unansweredAnswers);
         reset();
     }
 }
@@ -65,12 +120,10 @@ function decrement() {
 function reset() {
     clearInterval(intervalId);
     nextQuestion();
-    number = 10;
+    number = 15;
     $("#display").html("<h1>" + number + "</h1>");
     intervalId = setInterval(decrement, 1000);
     if (count === questions.length) {
-        //count = 0;
-        //console.log("game should stop here");
         stopTrivia();
     }
 }
@@ -79,7 +132,11 @@ function reset() {
 function displayQuestion() {
     $("#question").html(questions[count].text);
     // Code to dynamically display answer choices
-    $(".answer-choices").html("<br><p>" + questions[count].choices[0] + "</p><br><p>" + questions[count].choices[1] + "</p><br><p>" + questions[count].choices[2] + "</p><br><p>" + questions[count].choices[3] + "</p>");
+    $("#recap-heading").html("Trivia Recap");
+    $(".answer-choices").html("<br><br><h4>" + questions[count].choices[0] + "</h4><br><h4>" + questions[count].choices[1] + "</h4><br><h4>" + questions[count].choices[2] + "</h4><br><h4>" + questions[count].choices[3] + "</h4>");
+    if (count === questions.length) {
+        stopTrivia();
+    }
 }
 
 
@@ -87,30 +144,31 @@ function displayQuestion() {
 // Generate Next Question/loading Function
 function nextQuestion() {
     count++;
-    console.log("count is " + count + ".");
     $("#question").html("<img src = 'assets/images/loading.gif' width='100px'>");
     setTimeout(displayQuestion, 1000);
+}
+
+function compareAnswers() {
+    if (userChoice === questions[count].answer) {
+        $(".answer-choices").html("<img src= 'assets/images/Probst-win.gif' height='auto' width='300px'><br><br>").append("<h3>CORRECT</h3>");
+        correctAnswers++;
+        reset();
+    }
+    else {
+        $(".answer-choices").html("<img src= 'assets/images/Probst-wow.gif' height='auto' width='300px'><br><br>").append("<h3>WRONG<h/3>");
+        //$(".answer-choices").html("<h3>You chose poorly!</h3><br><h4>" + questions[count].answer + " was the correct answer!</h4>");
+        incorrectAnswers++;
+        reset();
+    }
 }
 
 // Stop Game Function
 function stopTrivia() {
     clearInterval(intervalId);
-    $("#question").html("<h1>Out of 20 total questions, you answered:</h1><br><p>" + correctAnswers + " questions correctly.</p><p>" + incorrectAnswers + " questions incorrectly.</p><br><p>You failed to answer " + unansweredAnswers + " questions.</p>");
-}
-
-function compareAnswers() {
-    if (userChoice === questions[count].answer) {
-        console.log("correct");
-        correctAnswers++;
-        console.log("questions answered correctly " + correctAnswers);
-        reset();
-    }
-    else {
-        console.log("incorrect");
-        incorrectAnswers++;
-        console.log("questions answered incorrectly " + incorrectAnswers);
-        reset();
-    }
+    $("#question").html("<h1>Trivia Totals</h1><h3> 20 total questions:</h3><h3>" + correctAnswers + " correct.</h3><h3>" + incorrectAnswers + " incorrect.</h3><h3>" + unansweredAnswers + " unanswered.</h3><br><button class='restart'>Restart</button>");
+    $(".answer-choices").html("<img src= 'assets/images/Tribe-has-spoken.gif' height='auto' width='300px'><br><br>").append("<h3>NO MORE QUESTIONS<h/3>");
+    return;
+    //$(".restart").on("click", restartGame);
 }
 
 
@@ -125,7 +183,6 @@ $(document).ready(function() {
         userChoice = $(this).attr("value");
         console.log("user picked " + userChoice + ".");
         compareAnswers();
-
     });
     
 
